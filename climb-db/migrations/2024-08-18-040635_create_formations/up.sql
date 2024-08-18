44b797b57d15654adc08c279856f0c5869aeb621
Logan Grosz <logan.grosz@gmail.com>
2024-08-17 22:14:34 -0600

    feat(db): add formations table

---
+++ climb-db/migrations/2024-08-18-040635_create_formations/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+CREATE TABLE "formations"(
+	"id" SERIAL NOT NULL PRIMARY KEY,
+	"names" TEXT[] NOT NULL
+);
+
--- climb-db/src/models.rs
+++ climb-db/src/models.rs
@@ -13,3 +13,17 @@ pub struct Climb {
 pub struct NewClimb {
     pub names: Vec<Option<String>>,
 }
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::formations)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Formation {
+    pub id: i32,
+    pub names: Vec<Option<String>>,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::formations)]
+pub struct NewFormation {
+    pub names: Vec<Option<String>>,
+}
