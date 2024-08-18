19e4bfc8a061c56432d8ee278aea3b1030238c08
Logan Grosz <logan.grosz@gmail.com>
2024-08-17 22:33:19 -0600

    feat(db): add areas table

--- climb-db/src/models.rs
+++ climb-db/src/models.rs
@@ -1,6 +1,20 @@
 use diesel::prelude::*;
 
 #[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::areas)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Area {
+    pub id: i32,
+    pub names: Vec<Option<String>>,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::areas)]
+pub struct NewArea {
+    pub names: Vec<Option<String>>,
+}
+
+#[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::schema::climbs)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Climb {
