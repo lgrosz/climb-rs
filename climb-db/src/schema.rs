a525759e31a21f30f6d50388f8a1855514769729
Logan Grosz <logan.grosz@gmail.com>
2024-08-17 22:14:34 -0600

    feat(db): add formations table

--- climb-db/src/schema.rs
+++ climb-db/src/schema.rs
@@ -6,3 +6,15 @@ diesel::table! {
         names -> Array<Nullable<Text>>,
     }
 }
+
+diesel::table! {
+    formations (id) {
+        id -> Int4,
+        names -> Array<Nullable<Text>>,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    climbs,
+    formations,
+);
