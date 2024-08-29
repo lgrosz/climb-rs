ce80c68b1eebf08f803c5b62b1d17aa4066fb0e0
Logan Grosz <logan.grosz@gmail.com>
2024-08-28 22:40:04 -0600

    refactor(db): remove redundant fk constraints
    Removes redundant foreign key constraints. Single-column "REFERENCES" do
    the same job as explicit FOREIGN KEY constraints in these scenarios and
    provide better readability.

--- climb-db/migrations/2024-08-26-041136_create_formation_belongs_to/up.sql
+++ climb-db/migrations/2024-08-26-041136_create_formation_belongs_to/up.sql
@@ -1,10 +1,8 @@
 -- Your SQL goes here
 CREATE TABLE formation_belongs_to (
     formation_id INTEGER PRIMARY KEY REFERENCES formations(id) ON DELETE CASCADE,
-    area_id INTEGER,
-    super_formation_id INTEGER,
-    CONSTRAINT fk_areas FOREIGN KEY (area_id) REFERENCES areas(id) ON DELETE RESTRICT,
-    CONSTRAINT fk_formations FOREIGN KEY (super_formation_id) REFERENCES formations(id) ON DELETE RESTRICT,
+    area_id INTEGER REFERENCES areas(id) ON DELETE RESTRICT,
+    super_formation_id INTEGER REFERENCES formations(id) ON DELETE RESTRICT,
     CHECK (
         (super_formation_id IS NOT NULL AND area_id IS NULL) OR
         (super_formation_id IS NULL AND area_id IS NOT NULL)
