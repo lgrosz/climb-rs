82902e5dc1d10733077b0f39f0caed4a63b35113
Logan Grosz <logan.grosz@gmail.com>
2024-09-06 21:26:58 -0600

    feat(db): add ascent tables

---
+++ climb-db/migrations/2024-09-07-031620_add_ascents/down.sql
@@ -0,0 +1,5 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE IF EXISTS "ascent_grades";
+DROP TABLE IF EXISTS "ascent_parties";
+DROP TABLE IF EXISTS "ascents";
+DROP TABLE IF EXISTS "climbers";
